a5bb1c17fcd1cdcc0d1ca5732915b0ba384312e9
shiva_4329 <sambasivaraokandi@gmail.com>
2024-05-02 21:12:27 +0530

    may 02

---
+++ Escape_Sequences/Escape_sqquence.py
@@ -0,0 +1,71 @@
+# Escape seqences\characters = followd by \ 
+
+
+#\' = single quote
+
+#\\ = skips the next value after 1st slash
+
+# \n = next line
+
+# \t = tab space
+
+# \r = carrige return
+
+# \b = back Space
+
+# \ooo = otal Value
+
+# \xhh = hex value
+
+
+
+
+print('hello\nworld') # \n - places value into next line
+
+print('hello\tworld') # \t - provides the tab space
+
+print('hello\\world') # \\ - when we are using \\ one of slash will be skipped
+
+print('hello\\\world') 
+
+print('hello\\\\world') 
+
+
+print('hello\\\\\world') 
+
+print('hello\\\\\\world') 
+
+
+
+# rama's book is in the class - by using single quotes
+
+print('rama\'s book is in the class') # here \' converst ' into string
+
+# the book belongs to \sita\
+
+print('the book belongs to \\sita\\')
+
+
+
+# \\India is my country\\
+
+print('\\\\India is my country\\\\')
+
+
+# hello im from \\America\\\, this book is from miller's collection
+
+
+print('hello im from \\\\America\\\\\\, this book is from millers\'s collection')
+
+# 'India'
+
+print('\'India\'')
+
+
+# Welcome to Python\'s \\world\\\
+
+
+# \\Miller is a \\wise person
+
+
+# "Sita" is a brave \\\\girl\\\\ in the class -- try with using double quotes
